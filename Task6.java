f64610b91249dc430dcc780043808e016e55c214
dariaost19 <dariaost19@gmail.com>
2023-01-16 21:32:39 -0500

    updatedHW8

--- Task6.java
+++ Task6.java
@@ -6,17 +6,22 @@ public class Task6 {
     public static void main(String[] args) {
         String[] country = {"USA", "Ukraine", "China", "Italy", "France"};
         String[] capitals={"Washighton DC", "Kyiv" ,"Beijing","Rome","Paris"};
-        System.out.println(country[0]+ " - " +capitals[0]);
-        System.out.println(country[1] +" - " +capitals[1]);
-        System.out.println(country[2]+ " - "+capitals[2]);
-        System.out.println(country[3]+ " - "+ capitals[3]);
-        System.out.println(country[4]+" - "+capitals[4]);
+
+        for(int i=0;i<country.length;i++){
+            System.out.println("The capital of " +country[i]+ " is " + capitals[i]);
+        }
+
+
 
 
             }
 
 
-        }
+    }
+
+
+
+
 
 
 
